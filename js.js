b3110b91285476dda0cdd60e11d1c91fa7e5be11
BugsDev2 <msherlock246@gmail.com>
2022-11-22 22:41:26 +0530

    Added two buttons. One for colormode where the user can pick a color to draw. The second one is rainbow mode where a random color is used for each div in the grid container.

--- js.js
+++ js.js
@@ -1,18 +1,43 @@
 const gridContainer = document.querySelector('.grid-container');
 const h2Display = document.getElementById('display');
 const range = document.getElementById('slider');
-document.addEventListener('contextmenu', e => e.preventDefault());
+const randomButton = document.getElementById('random-button');
+const inputColor = document.querySelector('input[type="color"]');
+let check;
 
+console.log(randomButton.hasAttribute('class'));
+//~ document.addEventListener('contextmenu', e => e.preventDefault());
+
+// random function to randomize stuff.
 function random(num) {
 	return Math.floor(Math.random()*num);
 }
+
+
+// inputColor button toggle
+inputColor.addEventListener('click', e => {
+	if (e.target.parentElement.classList != 'selected') {
+		randomButton.classList.remove('selected');
+		inputColor.parentElement.classList.add('selected');
+	}	
+})
+
+// randomButton button toggle
+randomButton.addEventListener('click', e => {
+	if (!randomButton.classList != 'selected') {
+		randomButton.classList.add('selected');
+		inputColor.parentElement.classList.remove('selected');
+	}
+})
+
+
 
 function run() {
 	if (gridContainer.childElementCount != 0) {
 		const deleteDiv = document.querySelectorAll('.grid-container div');
 		let refreshErr;
 		deleteDiv.forEach(item => {
-			(item.classList == "mouse-hover") ? refreshErr = true : refreshErr == false;
+			item.hasAttribute('style') ? refreshErr = true : refreshErr == false;
 		});
 		if (refreshErr) {
 			if (confirm('This will clear the canvas. Do you want to continue?')) {
@@ -23,6 +48,9 @@ function run() {
 		}
 		
 	}
+	
+	
+
 	
 	h2Display.textContent = range.value + ' x ' + range.value;
 	
@@ -38,12 +66,20 @@ function run() {
 		div.setAttribute('draggable', 'false');
 		div.addEventListener('mouseenter', clickhover);
 	})
-
+	
+	
+	
 	function clickhover(e){
+		randomButton.classList == 'selected' ? check = true : check = false;
 		if (e.buttons == 1) {
-			this.classList.add('mouse-hover');
+			if (check) {
+				this.setAttribute('style', `background-color: hsl(${random(361)},100%, 50%)`);
+			} else {
+				this.style.backgroundColor = inputColor.value;
+			}
+			
 		} else if (e.buttons == 2) {
-			this.classList.remove('mouse-hover');
+			this.removeAttribute('style');
 		}
 		
 	}
